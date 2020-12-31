b0d6443b7f81b5091c3ce19f785b90f0cc58108e
Ingo Karkat <swdev@ingo-karkat.de>
2020-12-31 12:13:53 +0100

    Tests: Restructure fixture

--- tests/addOrUpdateBlock/temp.bash
+++ tests/addOrUpdateBlock/temp.bash
@@ -2,10 +2,10 @@
 
 export FRESH="${BATS_TEST_DIRNAME}/fresh.txt"
 export EXISTING="${BATS_TEST_DIRNAME}/existing.txt"
-export BAD="${BATS_TEST_DIRNAME}/bad.txt"
+export ANOTHER="${BATS_TEST_DIRNAME}/another.txt"
 export FILE="${BATS_TMPDIR}/fresh.txt"
 export FILE2="${BATS_TMPDIR}/existing.txt"
-export FILE3="${BATS_TMPDIR}/bad.txt"
+export FILE3="${BATS_TMPDIR}/another.txt"
 export NONE="${BATS_TMPDIR}/none.txt"
 export NONE2="${BATS_TMPDIR}/none2.txt"
 
@@ -13,7 +13,7 @@ tempSetup()
 {
     cp -f "$FRESH" "$FILE"
     cp -f "$EXISTING" "$FILE2"
-    cp -f "$BAD" "$FILE3"
+    cp -f "$ANOTHER" "$FILE3"
     rm -f "$NONE" "$NONE2" 2>/dev/null
 }
 setup()
