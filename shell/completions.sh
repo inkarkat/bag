55102093b84bfbcc3034a544de73ff9b55b17236
Ingo Karkat <swdev@ingo-karkat.de>
2022-05-09 10:10:55 +0200

    bag: ENH: Add command-line completion

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,3 +1,23 @@
-#!/bin/sh source-this-script
+#!/bin/bash source-this-script
 
 completeAsCommand bag-consume onbag
+
+_bag_complete()
+{
+    local IFS=$'\n'
+    local cur args opts
+
+    opts='set --prepend add -a --append -B --no-backup -x --transactional list print -p --print -n --lines -q --quiet pop --pop --delete-empty peek --peek delete -d --delete undo -u --undo'
+    opts="${opts// /$'\n'}"
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    readarray -t args < <(compgen -c bag- 2>/dev/null)
+    args=("${args[@]#bag-}")
+    args=("${args[@]}" "${args[@]/#/--}")
+
+    readarray -t COMPREPLY < <(compgen -W "${opts}${args[*]:+${opts:+$'\n'}}${args[*]}" -- "$cur")
+    [ ${#COMPREPLY[@]} -gt 0 ] && readarray -t COMPREPLY < <(printf "%q\n" "${COMPREPLY[@]}")
+    return 0
+}
+complete -F _bag_complete bag
