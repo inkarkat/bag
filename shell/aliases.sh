85b3a846d0889162a10ab5f887ef1c8cb975d9c1
Ingo Karkat <swdev@ingo-karkat.de>
2026-05-07 15:58:30 +0200

    [on]bag[-consume]: Minor: Update alias support through runWithPrompt
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release, Bats 1.10.0

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,12 +1,14 @@
 #!/bin/sh source-this-script
 
-addAliasSupport bag-consume \
+eval "$(runWithPrompt --addAliasSupport bag-consume \
     'ta' \
-    'transactional|worst-status|all|print|bare|no-capture-output|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|parallel|exit-on-success|exit-on-failure|prepend-on-failure|append-on-failure' \
+    'transactional|all|print|bare|no-capture-output|parallel|prepend-on-failure|append-on-failure' \
     'nls' \
-    'count|max-lines|stop-on|initial-status|prefix-command|prefix-command-command|rate-limit|exit-on|prepend-on|prepend-unless|prepend-before|prepend-after|append-on|append-unless|append-before|append-after'
-addAliasSupport onbag \
+    'count|max-lines|stop-on|prefix-command|prefix-command-command|rate-limit|prepend-on|prepend-unless|prepend-before|prepend-after|append-on|append-unless|append-before|append-after'
+)"
+eval "$(runWithPrompt --addAliasSupport onbag \
     't1S' \
-    'transactional|ignore-existing|stop-on-empty|print|bare|no-capture-output|trailing-prompt|no-trailing-prompt|no-output-no-prompt||no-output-no-error|emulate-prompt|parallel|exit-on-success|exit-on-failure|prepend-on-failure|append-on-failure' \
+    'transactional|ignore-existing|stop-on-empty|print|bare|no-capture-output|parallel|prepend-on-failure|append-on-failure' \
     'nilst' \
-    'count|interval|max-lines|stop-on|stop-after|initial-status|prefix-command|prefix-command-command|rate-limit|exit-on|prepend-on|prepend-unless|prepend-before|prepend-after|append-on|append-unless|append-before|append-after'
+    'count|interval|max-lines|stop-on|stop-after|prefix-command|prefix-command-command|rate-limit|prepend-on|prepend-unless|prepend-before|prepend-after|append-on|append-unless|append-before|append-after'
+)"
